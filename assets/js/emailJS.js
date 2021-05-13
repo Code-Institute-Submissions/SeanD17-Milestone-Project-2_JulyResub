405f3ae15c53bcb7d7d2ccebe5c409b490cbfb2c
Sean Dolan <seandolan53@yahoo.com>
2021-05-13 20:29:08 +0000

    Fixed the form in the contact html page

---
+++ assets/js/emailJS.js
@@ -0,0 +1,23 @@
+
+(function () {
+    emailjs.init("user_Z3NhniKVmxQR4ouXUECcZ");
+})();
+
+function sendMail(contactForm) {
+    emailjs.send("service_m0pgoxr", "template_8t12twf", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.email.value,
+            "from_hotel": contactForm.hotel.value,
+            "from_comment": contactForm.comment.value,
+            "from_people": contactForm.people.value,
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error)
+            }
+        );
+    return false;
+}
